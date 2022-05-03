e30303b5c5b5290f7a274d11b01f31c1ec3dc06a
pbaki <pbaki999@gmail.com>
2022-05-03 18:09:11 +0200

    Added button with function that prompt window for number of grids and inserted before gridContainer, added random rgb to generate for each grid hover

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,28 +1,43 @@
-
 let gridContainer = document.createElement("div");
 gridContainer.setAttribute("id", "Grid-container");
 document.body.appendChild(gridContainer);
+let button = document.createElement("button");
+button.innerText = "Click Me";
+document.body.insertBefore(button, gridContainer);
+button.addEventListener("click", showGrids);
+grid(10);
 
+function randomColor(){
+    return color = Math.floor(Math.random() * 255) + 1;
+}
 function grid(squares){
-    gridContainer.style.background = "pink";
     gridContainer.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
     for (let columns = 0; columns < squares; columns++){
         let column = document.createElement("div");
+        column.addEventListener("mouseover", () => {column.style.backgroundColor = "rgb(" + randomColor()+ "," + randomColor() + "," + randomColor() + ")"});
         gridContainer.appendChild(column);
+
     }
     rowSquares = squares * squares - squares
     for (let rows = 0 ; rows < rowSquares; rows++){
         let row = document.createElement("div");
+        row.addEventListener("mouseover", () => {row.style.backgroundColor = "rgb(" + randomColor() + "," + randomColor() + "," + randomColor() + ")"});
         gridContainer.appendChild(row);
     }
     
 }
 
-grid(4);
-
-
-
-
+function showGrids(){
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+      };
+    let inputGrid = parseInt(prompt("Enter grid number (1-100):"));
+    if (inputGrid < 1 || inputGrid > 100) {
+        alert("Not good, must enter again");
+        inputGrid = parseInt(prompt("Enter grid number (1-100):"));
+    }
+    grid(inputGrid);
+}
 
 
 
